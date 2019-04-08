74b7d9eb6ab11b48e2d60176b47e1373a4cc4dc4
bkdwei <bkdwei@163.com>
2019-04-08 20:33:50 +0800

    初步完成复制功能

--- kdFileFinder/script/copy.py
+++ kdFileFinder/script/copy.py
@@ -2,45 +2,24 @@
 Created on 2019年3月26日
 
 @author: bkd
+
+复制功能，可以和其他程序进行复制粘贴操作
 '''
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtCore import QUrl,QMimeData
-from os.path import join
+from PyQt5.QtCore import QMimeData
 
 class copy:
     def execute(self,script_variable):
-        clipboard = QApplication.clipboard()
-        mimeData = clipboard.mimeData()
-        if mimeData.hasUrls():
-            print("准备粘贴:" + clipboard.text(),mimeData.formats())
-            urls = mimeData.urls()
-            for url in urls:
-                print("mimeData:" ,url.url() )
-            path = clipboard.text()
-            item = {}         
-            
         cur_path = script_variable["cur_path"]
         file_list = script_variable["file_list"]
-        print("in copy ",file_list)
-        urls = [ ]
-        for file in file_list :
-#             url = QUrl(join(cur_path,file.strip()).replace("%5C",""))
-            url = QUrl("file:///" + cur_path + "/" +file)
-#             url.set
-            urls.append(url)
-        mimeData = QMimeData()
-#         mimeData.setUrls(urls)
-#         mimeData.setData("text/uri-list",str("file:///E:/tmpwork/kdssh").encode())
-        files1 = ["file:///" +  cur_path + "/" + f for f in file_list]
-        files = "\n".join(files1)
-        mimeData.setData("text/uri-list",files.encode())
         
+        files = ""
+        for f in file_list :
+            files += "file://" +  cur_path + "/" + f + "\r\n"
+        
+        mimeData = QMimeData()
+        mimeData.setData("text/uri-list",files.encode())
+        clipboard = QApplication.clipboard()
         clipboard.setMimeData(mimeData)
-#         
-
-# QClipboard* cb = QApplication::clipboard();
-# QDir dir("contracts/" + item->text(0));
-# 
-# QMimeData* mimeData = new QMimeData();
-# mimeData->setData("text/uri-list",QString("file:///" + dir.absolutePath()).toLatin1());
-# cb->setMimeData(mimeData);
+        
+        print("成功复制到剪切板" , files)
