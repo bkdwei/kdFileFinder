8cffdf580885164760c71f42b6e84d5a4c23293d
bkdwei <bkdwei@163.com>
2019-04-10 10:25:56 +0800

    修正删除失败的bug

--- kdFileFinder/script/delete.py
+++ kdFileFinder/script/delete.py
@@ -4,16 +4,20 @@ Created on 2019年3月26日
 @author: bkd
 '''
 from os import remove
+from os.path import join
 from shutil import rmtree
 from os.path import isfile,isdir
 class delete:
     def execute(self,script_variable):
-        file_path = script_variable["cur_item"]
-        if isfile(file_path) :
-            remove(file_path)
-            print("删除文件成功，" + file_path)
-        elif isdir(file_path) :
-            rmtree(file_path)
-            print("删除目录成功，" + file_path)
-        else :
-            print("无法删除的项目")    
+            cur_path = script_variable["cur_path"]
+            file_list = script_variable["file_list"]
+            for file in file_list :
+                file_path = join(cur_path,file)
+                if isfile(file_path) :
+                    remove(file_path)
+                    print("删除文件成功，" + file_path)
+                elif isdir(file_path) :
+                    rmtree(file_path)
+                    print("删除目录成功，" + file_path)
+                else :
+                    print("无法删除的项目")    
--- kdFileFinder/script/make_archive.py
+++ kdFileFinder/script/make_archive.py
@@ -4,7 +4,10 @@ Created on 2019年3月26日
 @author: bkd
 '''
 from shutil import make_archive as ma
+from os.path import join
+
 class make_archive:
     def execute(self,script_variable):
-        cur_item = script_variable["cur_item"]
-        ma(cur_item,"zip",root_dir=cur_item)
+        cur_item = join(script_variable["cur_path"],script_variable["file_list"][0])
+#         ma(cur_item,"zip",root_dir=cur_item)
+        ma(cur_item,"zip")
